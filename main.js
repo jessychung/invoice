8c30c1bea2e3f501b51069841aed186a14b61dcc
Jessy Chung <jessyc@thentia.com>
2017-02-21 20:51:26 -0500

    init

--- main.js
+++ main.js
@@ -12,12 +12,14 @@ $(function () {
 
     var idCount = 0;
 
+
     var storedItems = JSON.parse(localStorage.getItem('griditems'));
     console.log(storedItems);
 
 
     if(storedItems) {
         for(var i = 0; i < storedItems.length; i++) {
+            idCount++;
             var itemid = storedItems[i].id;
             var itemhtml = storedItems[i].value;
             $('.grid').append(itemhtml);
@@ -64,13 +66,33 @@ $(function () {
             };
             griditems.push(eachitem);
         })
-        localStorage.setItem('griditems', JSON.stringify(griditems.slice(1)));
+        localStorage.setItem('griditems', JSON.stringify(griditems));
         $('.grid-item').unwrap();
 
     });
 
 
 
+    var newitem = `<div class="row grid-item" id="griditem">
+                   <div class="col-md-6">
+                   <input type="text" class="form-control grid-input" placeholder="Enter Item Description" id="item"></div>
+                    <div class="col-md-2">
+                        <input type="number" min="0" class="form-control grid-input item-qty" id="qty">
+                    </div>
+                    <div class="col-md-2">
+                        <input type="number" min="0" class="form-control grid-input item-price" id="price">
+                    </div>
+                    <div class="col-md-2">
+                        <span class="item-amount">$0.00</span>
+                    </div>
+                    <div class="delete-item">
+                        <i class="fa fa-trash-o" aria-hidden="true"></i>
+                    </div>
+                    </div>`;
+
+    if($('.grid').children().length == 0) {
+        $('.grid').append(newitem);
+    }
 
 
 
@@ -95,8 +117,7 @@ $(function () {
         $('.grid').append(newitem);
     });
 
-    var itemqty;
-    var itemprice;
+
 
     $(document).on('mouseenter', '.grid-item', function()
     {
@@ -110,22 +131,66 @@ $(function () {
         $(this).find('.delete-item').hide();
     });
 
+    var count = 0;
+
     $(document).on('click', '.delete-item', function () {
         $(this).parent('.grid-item').remove();
+        $('.grid-item').each(function () {
+            count++;
+            $(this).attr('id', `griditem${count}`);
+            $(this).find('.item-qty').attr('id', `qty${count}`);
+            $(this).find('.item-price').attr('id', `price${count}`);
+        })
+
+
     });
 
-    $(document).on('keyup', '.item-qty', function () {
-        itemqty = parseFloat($(this).val());
-    });
+    $(document).on('keyup change', '.item-qty', function () {
 
-    $(document).on('keyup', '.item-price', function () {
-        itemprice = parseFloat($(this).val());
-        var amount = Math.round(itemprice * itemqty).toFixed(2);
+        var itemprice = parseFloat($(this).parent().siblings('div').find('.item-price').val());
+        var itemqty = parseFloat($(this).val());
 
+        if(!itemprice) {
+            itemprice = 0;
+        } else {
+            itemprice = parseFloat($(this).parent().siblings('div').find('.item-price').val());
+        }
+
+        if(!itemqty) {
+            itemqty = 0;
+        } else {
+            itemqty = parseFloat($(this).val());
+        }
+
+        console.log(itemprice);
         console.log(itemqty);
 
+        var amount = Math.round(itemprice * itemqty).toFixed(2);
         $(this).parent('div').siblings('div').find('.item-amount').text('$' + amount);
+    });
 
+    $(document).on('keyup change', '.item-price', function () {
+
+        var itemprice = parseFloat($(this).val());
+        var itemqty = parseFloat($(this).parent().siblings('div').find('.item-qty').val());
+
+        if(!itemprice) {
+            itemprice = 0;
+        } else {
+            itemprice = parseFloat($(this).val());
+        }
+
+        if(!itemqty) {
+            itemqty = 0;
+        } else {
+            itemqty = parseFloat($(this).parent().siblings('div').find('.item-qty').val());
+        }
+
+        console.log(itemprice);
+        console.log(itemqty);
+
+        var amount = Math.round(itemprice * itemqty).toFixed(2);
+        $(this).parent('div').siblings('div').find('.item-amount').text('$' + amount);
     });
 
 
